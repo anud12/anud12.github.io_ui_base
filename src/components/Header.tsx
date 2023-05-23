073571457130d041eb41e80e601a7defa7339053
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-05-23 16:11:52 +0300

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,21 @@ type Props = PropsWithChildren<{}> & {
 export const Header = (props: Props) => {
     return <Container className={"header-content"}>
         <div className="header-return">
-            <a href={props.backHref}>Back</a>
+            <a href={props.backHref}>
+                <span className="material-symbols-outlined">
+                    arrow_back
+                </span>
+            </a>
         </div>
         <div className="header-title">
             {props.children}
+        </div>
+        <div className="header-login">
+            <button>
+                <span className="material-symbols-outlined">
+                    login
+                </span>
+            </button>
         </div>
     </Container>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import React, { CSSProperties } from "react";
-import { Page } from "./Page";
-import { Header } from "./Header";
-import { Comment } from "./Comment";
-import { Container } from "./Container";
+import { Page } from "./components/Page";
+import { Header } from "./components/Header";
+import { Comment } from "./components/Comment";
+import { Container } from "./components/Container";
 const time = new Date().toISOString();
 module.exports = (
+
     <html>
         <head>
-            <link href={"../src/index.css"} type="text/css" rel="stylesheet" />
+            <link href={"../src/main.css"} type="text/css" rel="stylesheet" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,100,0,0" />
+
+            <link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@500&display=swap" rel="stylesheet" />
+
         </head>
         <body style={{ "--primary": "#70a3c7" } as CSSProperties}>
             <Comment>{time}</Comment>
