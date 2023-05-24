f20f967322f124f9e371e9db159799fd6d94ed5b
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-05-24 17:52:03 +0300

    commit

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,29 +1,20 @@
-import React from "react"
+import React, { CSSProperties } from "react"
 import { Action } from "./Action"
 
 export const Table = () => {
     return <div className="table-container">
-        <table className="table">
-            <thead>
-                <th>
-                    <a href="./add" className="material-symbols-outlined">
-                        add_box
-                    </a>
-                </th>
-                {new Array(20).fill("").map((e, index) => <th>
-                    <span>Header {index}</span>
-                </th>)}
-            </thead>
-            <tbody>
-                {new Array(10).fill("").map((e, index) =>
-                    <tr>
-                        <td>Action {index}</td>
-                        {new Array(20).fill("").map((e, jndex) =>
-                            <td>Cell with long value maybe {index} - {jndex}</td>
-                        )}
-                    </tr>
-                )}
-            </tbody>
-        </table>
-    </div>
+        <div className="table" style={{ "--number-of-columns": "21", "--number-of-rows": "20" } as CSSProperties}>
+            {new Array(10).fill("").map((e, index) =>
+                <div className="row">
+                    {/* <button>First {index}</button> */}
+                    <Action />
+                    {
+                        new Array(20).fill("").map((e, jndex) =>
+                            <div>Cell with long value maybe item: {index} - property: {jndex}</div>
+                        )
+                    }
+                </div>
+            )}
+        </div>
+    </div >
 }
