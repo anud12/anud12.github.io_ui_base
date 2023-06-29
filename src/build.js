a05b783cb1214b0986f15ef25dffa0e7df97fec2
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-06-29 17:08:58 +0300

    commit

--- src/build.js
+++ src/build.js
@@ -1,2 +1,2 @@
-require("anud12.github.io_ui_compiler").JSX(`${__dirname}/index.tsx`, `${__dirname}/../index.html`);
-require("anud12.github.io_ui_compiler").JSX(`${__dirname}/index_add.tsx`, `${__dirname}/../add/index.html`);
+require("anud12.github.io_ui_compiler").JSX(`${__dirname}/index.tsx`, `${__dirname}/../index`);
+require("anud12.github.io_ui_compiler").JSX(`${__dirname}/index_add.tsx`, `${__dirname}/../add/index`);
