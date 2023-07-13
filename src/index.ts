e6cb172491f634044f17ca8f68996b987bf077d6
anud12 <supstanta_andu@yahoo.com>
2023-07-13 18:16:51 +0000

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,12 @@
-import React from "react";
-import { Page } from "./components/Page";
-import { Table } from "./components/Table";
-import { Form } from "./components/Form";
-import { Link } from "./components/atoms/Link";
-import { Input } from "./components/atoms/Input";
-import { Button } from "./components/atoms/Button";
+export { Page } from "./components/Page";
+export { Table } from "./components/Table";
+export { Form } from "./components/Form";
+export { Link } from "./components/atoms/Link";
+export { Input } from "./components/atoms/Input";
+export { Button } from "./components/atoms/Button";
 
 declare global {
     let google: any;
     let iziToast: any;
     let gapi: any;
-}
-export = {
-    Page,
-    Table,
-    Link,
-    Form,
-    Input,
-    Button,
 }
